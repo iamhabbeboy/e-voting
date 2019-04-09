52271902aab5052cfcb28e8ce5a45261feaf7aee
Azeez Abiodun <iamhabbeboy@gmail.com>
2019-04-09 10:01:57 +0100

    updated config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 
-   define('DB_PWD', 'root');
-   define('DB_USER', 'root');
-   define('DB_HOST', 'localhost');
-   define('DB_NAME', 'evoting');
+   define('DB_PWD', 'aA3!lL8%');
+   define('DB_USER', 'sql2287301');
+   define('DB_HOST', 'sql2.freemysqlhosting.net');
+   define('DB_NAME', 'sql2287301');
     
